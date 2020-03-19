b698783d9a552204d37809fcb7fbaa4c02e41b77
JooYoung <sana2dang@naver.com>
2020-03-19 12:39:49 +0900

    n64 script add
    n64 실행 스크립트 추가

---
+++ cfg/n64.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/mupen64plus/mupen64plus --resolution 320x240 --plugindir /opt/mupen64plus --gfx mupen64plus-video-rice.so --corelib /opt/mupen64plus/libmupen64plus.so.2 --datadir /opt/mupen64plus "$1"
